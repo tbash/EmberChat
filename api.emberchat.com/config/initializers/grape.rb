905c1c3f5412a765503d13cd5f1627a2c5b753a3
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-10-05 17:46:13 -0400

    Small cleanups

--- api.emberchat.com/config/initializers/grape.rb
+++ api.emberchat.com/config/initializers/grape.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url = "/api/v1/swagger_doc"
-GrapeSwaggerRails.options.app_name = "teebash"
+GrapeSwaggerRails.options.app_name = "EmberChat"
 GrapeSwaggerRails.options.app_url = "http://33.33.33.100:3000"
