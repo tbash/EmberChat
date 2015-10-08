beda6f73abd46139a94e816c6b7f27c66978f0eb
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-10-07 20:32:59 -0400

    bye DS_Store

--- emberchat.com/config/environment.js
+++ emberchat.com/config/environment.js
@@ -2,6 +2,15 @@
 
 module.exports = function(environment) {
   var ENV = {
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+      'font-src': "'self' data: use.typekit.net",
+      'connect-src': "'self' ws://localhost:49152 ws://0.0.0.0:49152",
+      'img-src': "'self' www.facebook.com p.typekit.net",
+      'style-src': "'self' 'unsafe-inline' use.typekit.net",
+      'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
+    },
     modulePrefix: 'ember-chat',
     environment: environment,
     baseURL: '/',
